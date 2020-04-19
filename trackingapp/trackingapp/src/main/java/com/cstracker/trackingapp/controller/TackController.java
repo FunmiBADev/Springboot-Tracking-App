eb97ae9ed83b3392064a893cf3c6b79f538a7f65
FunmiBADev <funmiba@hotmail.com>
2020-04-19 01:35:26 +0100

    post method for create task, removed the recurity from pom file

--- trackingapp/trackingapp/src/main/java/com/cstracker/trackingapp/controller/TackController.java
+++ trackingapp/trackingapp/src/main/java/com/cstracker/trackingapp/controller/TackController.java
@@ -2,11 +2,15 @@ package com.cstracker.trackingapp.controller;
 
 import com.cstracker.trackingapp.model.Task;
 import com.cstracker.trackingapp.repository.TaskRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collection;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -23,6 +27,22 @@ public class TackController {
     Collection<Task> tasks(){
         return taskRepo.findAll();
 //        select all from tasks
+    }
+
+//    Search task by id (can repeat by search by username, status, category, priority)
+    @GetMapping("/search_task/{task_id}")
+    ResponseEntity<?> getCategory(@PathVariable Long task_id){
+        Optional<Task> task = taskRepo.findById(task_id);
+        return task.map(response -> ResponseEntity.ok().body(response))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+
+    }
+
+    @PostMapping("/add_task")
+    ResponseEntity<Task> createTask(@Valid @RequestBody Task task) throws URISyntaxException {
+        Task result= taskRepo.save(task);
+        return ResponseEntity.created(new URI("/api/dashboard" + result.getTask_id())).body(result);
+
     }
 
 
